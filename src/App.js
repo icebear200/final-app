0aa000dfd1fa2bc2dc2171705fb3eaa4585f72b4
angie <angela_osei-ampadu@brown.edu>
2024-04-02 18:30:07 -0400

    pushing sort

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import {useState} from "react"
 import SongItem from './components/SongItem';
 import songData from './assets/song-data.json';
+// import {View, Picker} from 'react-native';
 
 songData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/"+ item.image;
 });
 
 function App() {
-
 const [favList, setFavList] = useState ([])
+const [sortedSongs, setSortedSongs] = useState(songData)
+const [filteredArtist, setFilteredArtist] = useState(songData)
 const favorite = (songName) => {
   if (!favList.includes(songName)){
   setFavList ((prevFavList) => [... prevFavList, songName]);
@@ -26,40 +28,55 @@ const finalFavList = () => {
   return final;
   
 }
+const sortSongs = () => {
+  const sortedSongsCopy = [...sortedSongs];
+  const sortedSongsNew = sortedSongsCopy.sort((a, b)=> a.name.localeCompare(b.name))
+  setSortedSongs(sortedSongsNew)
+
+  // const filterArtist = () => {
+  //   const filterArtistNew = filteredArtistCopy.filter(artist)
+  //   setFilteredArtist(filterArtistNew)
+  // }
+}
+
   return (
     <div className="App">
+      <div className="appTitle">  <h1>Angie's Mix  </h1></div>
+      <div class="songListContainer"> 
       <div class="SongList">   
+      <button onClick={sortSongs}>Sort Songs Alphabetically</button>
+      {/* <Picker> </Picker> */}
+    
       <header className="App-header">
         {/* <img src={image} className="App-logo" alt="logo" /> */}
        
-        {songData.map ((item, index) => (
+       
+
+       
+        {sortedSongs.map ((item, index) => (
           <SongItem data={item} key={index} favorite={() => favorite(item.name)}
           name = {item.name}
+          artist={item.artist}
+          length={item.length}
           genre= {item.genre}
           year = {item.year}
            />
         ))}
 
-{/* 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
       </header>
+
+      </div> 
 
       <div className="favSongList">
         <h2> Your Favorite Songs</h2>
         <u1> {favList.map((songName, index) =>
         <li key={index}> {songName}</li>)}</u1>
         <div> {finalFavList}</div>
-    
       </div>
       </div>
       </div>
+  
 
   
     
--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -2,11 +2,18 @@ export default function SongItem(props){
 
     
     return (
-        <div>     
+        <div>  
+        <div className="formatImage"> 
         <img class= "songImage" src = {props.data.image} />
         <div className= "itemInfo"> 
+        </div>   
+
         <div className="infoPadding"> 
+        <div className="songAndartist"> 
         <div className="title "> {props.data.name} </div> 
+        <div className="artist">{props.data.artist}</div>
+        </div>
+        <div className="length"> {props.data.length}</div>
         <div className="genre"> {props.data.genre} </div>
         <div className="year"> {props.data.year} 
          <button onClick={ () => props.favorite(props.data.name)}> Add To Favorites</button> 
