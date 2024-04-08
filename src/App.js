9d899639efa3ce954d72f4695dd6e650eec7cba7
angie <angela_osei-ampadu@brown.edu>
2024-04-07 23:41:26 -0400

    pushing final + readMe

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 
 const App = () => {
   return ( 
-    // added a class "content" to apply a maxwidth to it in App.css. this is centering all the contents on the page
     <div className="content">
       <Homepage/>
     </div>
